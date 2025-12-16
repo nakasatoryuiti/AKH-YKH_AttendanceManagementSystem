16c3d446cb98a4ea361515f8322810d1722d106d
nakasatoryuiti <homugutamu@gmail.com>
2025-12-16 16:27:30 +0900

    Add files via upload

---
+++ User.java
@@ -0,0 +1,17 @@
+package com.example.attendancesystem.model; // 修正
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    private String id;
+    private String password;
+    private String name;
+    private Integer roleId;
+}
